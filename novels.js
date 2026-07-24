70d345556ab5305af3218f44f871e23886f41817
nothingisreal888 <nothingisr3al888@gmail.com>
2026-07-24 14:00:44 -0500

    Created novels.js

---
+++ novels.js
@@ -0,0 +1,9 @@
+//List of all novels and their stuff
+
+export default Class Novel {
+  constructor(name, genre, date) {
+    this.name = name;
+    this.genre = genre;
+    this.date = date;
+  }
+}
