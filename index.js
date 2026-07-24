a1bac442f8e610cc4a516bb0eadd15d412d497e8
nothingisreal888 <nothingisr3al888@gmail.com>
2026-07-24 13:59:16 -0500

    Created index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+//Hopefully something interesting
+
+/*Class Novel {
+  constructor(name, genre, date) {
+    this.name = name;
+    this.genre = genre;
+    this.date = date;
+  }
+}*/
+
+import Novel from "./novels.js";
+novel1 = new Novel("Template", "None", "07-22-2026");
